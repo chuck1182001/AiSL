3ed3b96ef67ac548b666b6b09069197f41f2a82b
charlie <charlie.guischard@gmail.com>
2024-03-30 12:38:14 -0400

    run python script with node

--- Backend/aisl-express/routes/index.js
+++ Backend/aisl-express/routes/index.js
@@ -4,6 +4,11 @@ const port = 3001
 
 app.get('/', (req, res) => {
   // console.log(req.socket.localPort);
+  const spawn = require("child_process").spawn;
+  const pythonProcess = spawn('python',["./test.py"]);
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(data);
+  });
   console.log(req.socket.remotePort);
   res.send('Hello World!')
 })
