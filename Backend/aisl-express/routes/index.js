2e871f4b12a0b6dd9f471b700a27a7fc425fef97
Charlie Guishard <charlie.guischard@gmail.com>
2024-03-30 20:19:26 +0000

    editting backend to upload images

--- Backend/aisl-express/routes/index.js
+++ Backend/aisl-express/routes/index.js
@@ -1,23 +1,49 @@
 var express = require('express');
 var router = express.Router();
+const multer  = require('multer');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req, res, next) {
+// router.post('/', function(req, res, next) {
+//   // const spawn = require("child_process").spawn;
+//   // const pythonProcess = spawn('python',["./test.py"]);
+//   // pythonProcess.stdout.on('data', (data) => {
+//   //   console.log(data.toString());
+//   // });
+//   // pythonProcess.on('error', (err) => {
+//   //   console.log(err);
+//   // });
+//   // const test = req.params.image;
+//   // console.log(req.params);
+//   express.request.on('body', (data) => {
+//     console.log(data);
+//   });
+//   res.send('Hello World!');
+// });
 
-  // const spawn = require("child_process").spawn;
-  // const pythonProcess = spawn('python',["./test.py"]);
-  // pythonProcess.stdout.on('data', (data) => {
-  //   console.log(data.toString());
-  // });
-  // pythonProcess.on('error', (err) => {
-  //   console.log(err);
-  // });
-  const test = req.params.image;
-  console.log(test);
-  res.send('Hello World!');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/'); // specify the upload directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); // generate unique filename
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// Define route to handle POST request with image upload
+router.post('/image', upload.single('image'), (req, res) => {
+  // req.file contains information about the uploaded file
+  if (!req.file) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  // You can access the file details like req.file.path, req.file.originalname, etc.
+  // Here you can save the file path to a database or perform further processing
+
+  res.send('File uploaded successfully.');
 });
 
 module.exports = router;
